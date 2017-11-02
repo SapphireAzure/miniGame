f808c31b6540b2b5964a9d500f5357a33153a593
颜奕 <yanyi@yanyideMacBook-Pro.local>
2017-11-02 21:42:48 +0800

    try to make HPbar

---
+++ Assets/Scripts/UI/HpUpdate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HpUpdate : MonoBehaviour {
+	public PlayerQuality playerQuality;
+
+	public GameObject player;
+
+	public Scrollbar hpBar;
+
+	float currentHp;
+
+	float maxHp;
+
+	// Use this for initialization
+	void Start () {
+		//暂时无法绑定player的quality..
+		playerQuality = new PlayerQuality();
+	
+		maxHp = playerQuality.maxHp;
+
+		currentHp = playerQuality.HP;
+
+		hpBar.size = currentHp / maxHp;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		maxHp = playerQuality.maxHp;
+
+		currentHp = playerQuality.HP;
+			
+		hpBar.size = currentHp / maxHp;
+		
+	}
+}
